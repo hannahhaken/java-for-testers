946a9264e3ed8b6e718629cce3c31eb048f5b93f
Hannah Haken <hannahhaken@gmail.com>
2017-07-06 15:44:21 +0100

    Add tests in MyFirstTest. Create chap004testswithotherclasses class.

--- src/test/java/com/javafortesters/chap003myfirsttest/examples/MyFirstTest.java
+++ src/test/java/com/javafortesters/chap003myfirsttest/examples/MyFirstTest.java
@@ -1,4 +1,40 @@
 package com.javafortesters.chap003myfirsttest.examples;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class MyFirstTest {
+
+    @Test
+    public void canAddTwoPlusTwo(){
+
+        int answer = 2+2;
+
+        assertEquals("2+2=4", 4, answer);
+    }
+
+    @Test
+    public void canMinusTwoFromTwo(){
+
+        int answer = 0;
+
+        assertEquals("2-2=0", 0, answer);
+    }
+
+    @Test
+    public void canDivideFourByTwo(){
+
+        int answer = 2;
+
+        assertEquals("4/2=2", 2, answer);
+    }
+
+    @Test
+    public void canTimesTwoByTwo(){
+
+        int answer = 4;
+
+        assertEquals("2*2=4", 4, answer);
+    }
 }
--- src/test/java/com/javafortesters/chap004testswithotherclasses/examples/IntegerExamplesTest.java
+++ src/test/java/com/javafortesters/chap004testswithotherclasses/examples/IntegerExamplesTest.java
@@ -1,7 +1,35 @@
 package com.javafortesters.chap004testswithotherclasses.examples;
 
-/**
- * Created by Hannah Haken on 04/07/2017.
- */
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class IntegerExamplesTest {
+
+    @Test
+    public void integerExploration() {
+        Integer four = new Integer(4);
+        assertEquals("intValue returns 4", 4, four.intValue());
+
+        Integer five = new Integer("5");
+        assertEquals("intValue returns int 5", 5, five.intValue());
+
+        Integer six = 6;
+        assertEquals("autoboxing assignment for 6", 6, six.intValue());
+    }
+
+    @Test
+    public void canConvertIntToHex() {
+        assertEquals("hex 11 is b", "b", Integer.toHexString(11));
+    }
+
+    @Test
+    public void canCheckIntegerMinAndMaxValue(){
+
+        int minimumInt = -2147483648;
+        int maximumInt = 2147483647;
+
+        assertEquals("MIN_VALUE equals -2147483648", minimumInt, Integer.MIN_VALUE);
+        assertEquals("Integer.MAX_VALUE equals 2147483647", maximumInt, Integer.MAX_VALUE);
+    }
 }
