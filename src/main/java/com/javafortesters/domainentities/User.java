0e639196b8653a557d001b1ce142499c7e474be9
Hannah Haken <hannahhaken@gmail.com>
2017-07-13 16:55:21 +0100

    Add test classes chap005 and chap006
    Add classes domainentities and domainobject

---
+++ src/main/java/com/javafortesters/domainentities/User.java
@@ -0,0 +1,13 @@
+package com.javafortesters.domainentities;
+
+
+public class User {
+
+    private String username;
+    private String password;
+
+    public User(){
+        username = "username";
+        password = "password";
+    }
+}
